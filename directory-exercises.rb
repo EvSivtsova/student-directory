2548eb89467188299deeea3117530a37fd563fb2
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-06-10 10:27:44 +0000

    Amended the program to ask for the cohort

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -7,42 +7,38 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # asks for more infortmation
-    puts "What your age?"
-    age = gets.chomp
-    puts "What is your height?"
-    height = gets.chomp
-    puts "What's your country of birth?"
-    country_of_birth = gets.chomp
-    puts "What are you hobbies?"
-    hobbies = gets.chomp
-    # add the student hash to the array
-    students << {name: name, cohort: :november, age: age, height: height, hobbies: hobbies, country_of_birth: country_of_birth}
+    #ask for the cohort
+    month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    while true
+      puts "What's your cohort? Select month"
+      cohort = gets.chomp.capitalize
+      if month.include?(cohort)
+        # add the student hash to the array. Converts the cohort value to a symbol
+        students << {name: name, cohort: cohort.to_sym}
+        break
+      end
+    end
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  # return the array of students
+  # return the array pf students
   students
 end
 
 def print_header
-    puts "The students of Villains Academy".center(50)
-    puts "--------------".center(50)
+    puts "The students of Villains Academy"
+    puts "--------------"
 end
 
 def print(students)
-  count = 0
-  while count < students.count
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
-      count += 1
-    end
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(50)
+  puts "Overall, we have #{names.count} great students"
 end
 
 
