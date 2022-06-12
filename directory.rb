58c931370322321d1edbf417beb53dd8b3f9e724
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-06-12 17:43:06 +0000

    Fixed an issue with student_data method by using @name throughout the code

--- directory.rb
+++ directory.rb
@@ -45,14 +45,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  @name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while !@name.empty? do
     # add the student hash to the array
     student_data
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    @name = STDIN.gets.chomp
   end
 end
 
